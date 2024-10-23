38c12909d61863de23b0c7d9349a2a42fd43b683
Pleple5 <148486000+Pleple5@users.noreply.github.com>
2024-10-23 21:18:01 +0200

    Add files via upload

---
+++ php1.php
@@ -0,0 +1,23 @@
+<?php
+$a=100.56;  // zmienna
+var_dump($a);
+echo "<br>";
+print_r($a);
+echo "<br>";
+$b="Ala ma kota";
+$b .= 4;
+echo $b."<br>";
+var_dump($b);
+echo "<br>";
+$c=false;
+var_dump($c);
+echo "<br>";
+define("zzz",300);  // stala - brak znaku $
+echo zzz."<br>";
+echo "<br>".(2*2)."<br>";
+echo "<strong>Wartość</strong>";
+echo "<section><br></section>";
+echo "Wartość a=".$a."<br>";
+echo "Wartość b=".$b."<br>";
+print($a);
+?>
