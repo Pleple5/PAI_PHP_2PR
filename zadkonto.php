a60c60f114b504263bcc2972e4051cb2c7b6bbb1
Pleple5 <148486000+Pleple5@users.noreply.github.com>
2025-06-05 22:08:02 +0200

    zadkonto.php

--- zadkonto.php
+++ zadkonto.php
@@ -1,34 +1,86 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Konto Banowe</title>
-</head>
-<body>
-    <form method="post">
-        <button type="submit" name="wplac">Wpłać</button>
-        <button type="submit" name="wyplac">Wypłać</button>
-    </form>
-
 <?php
     // konto bankowe, metody w klasie stan konta konstruktor tworzy to konto z zerowym stanem, metoda wpłać, metoda wypłać i stan konta
     class kontobankowe
     {
-        private $stan;
-
-        public function __construct()
+        class KontoBankowe
         {
-            $this->stan=0;
-        }
-
-        public function wplac($kwota)
-        {
-            if ($kwota > 0){
-                $this->stan+=$kwota;
+            private $stan;
+        
+            public function __construct()
+            {
+                $this->stan = 0;
+            }
+        
+            public function wplac($kwota)
+            {
+                if ($kwota > 0)
+                {
+                    $this->stan += $kwota;
+                    return "Wpłacono $kwota zł";
+                }
+                return "Kwota musi być większa niż 0";
+            }
+        
+            public function wyplac($kwota) {
+                if ($kwota <= 0)
+                {
+                    return "Kwota musi być większa niż 0";
+                }
+                elseif ($kwota > $this->stan)
+                {
+                    return "Brak środków";
+                }
+                else
+                {
+                    $this->stan -= $kwota;
+                    return "Wypłacono $kwota zł";
+                }
+            }
+        
+            public function stanKonta() {
+                return $this->stan;
             }
         }
     }
-    ?>
+
+$konto = new KontoBankowe();
+$komunikat = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $kwota = floatval($_POST['kwota']);
+
+    if (isset($_POST['wplac'])) {
+        $komunikat = $konto->wplac($kwota);
+    }
+
+    if (isset($_POST['wyplac'])) {
+        $komunikat = $konto->wyplac($kwota);
+    }
+
+    $stan = $konto->stanKonta();
+        else {
+    $stan = $konto->stanKonta();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+     <meta charset="UTF-8">
+    <title>Konto bankowe</title>
+</head>
+<body>
+    <h1>Konto Bankowe</h1>
+    <p><strong>Stan konta:</strong> <?php echo $konto->stanKonta(); ?> zł</p>
+    
+    <form method="post">
+        <label for="kwota">Kwota:</label>
+        <input type="number" step="0.01" name="kwota" required>
+        <br><br>
+        <button type="submit" name="wplac">Wpłać</button>
+        <button type="submit" name="wyplac">Wypłać</button>
+    </form>
+    
+    <p style="color:green;"><?php echo $komunikat; ?></p>
 </body>
 </html>
